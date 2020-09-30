e8a0e005c7347b64388029492802793083e7c207
RYOTA <kobaryo04ysh@icloud.com>
2020-10-01 00:07:16 +0900

    transrate to JP

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+// 年月日をyyyy/mm/dd/曜日で表示するfunction
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear(),
+        dayOfTheWeek = d.getDay(),
+        dayOfTheWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfTheWeek];
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    // 戻り値にyyyy/mm/dd/曜日を指定
+    return [year, month, day, dayOfTheWeekStr].join('/');
+}
+// 日付の取得
 const date = new Date();
 
 const renderCalendar = () => {
+    // 取得した月の日付を指定
     date.setDate(1);
 
     const monthDays = document.querySelector(".days");
@@ -11,6 +29,7 @@ const renderCalendar = () => {
         0
     ).getDate();
     
+    // getMonthに+１しないのは前月だから
     const prevLastDay = new Date(
         date.getFullYear(), 
         date.getMonth(), 
@@ -19,38 +38,49 @@ const renderCalendar = () => {
     
     const firstDayIndex = date.getDay();
     
+    //取得した月の最終日の曜日を数値で取得
     const lastDayIndex = new Date(
         date.getFullYear(),
         date.getMonth() + 1,
         0
     ).getDay();
     
+    // 同じ週で翌月に何日あるのかを取得
     const nextDays = 7 - lastDayIndex - 1;
     
     const months = [
-        "January",
-        "February",
-        "Marth",
-        "April",
-        "May",
-        "June",
-        "July",
-        "Augst",
-        "September",
-        "October",
-        "November",
-        "December",
+        "1月",
+        "2月",
+        "3月",
+        "4月",
+        "5月",
+        "6月",
+        "7月",
+        "8月",
+        "9月",
+        "10月",
+        "11月",
+        "12月"
     ];
     
+    // h1タグ(.date h1)に月を取得し代入
     document.querySelector('.date h1').innerHTML
     = months[date.getMonth()];
-    
+
+    // 年月日曜日
+    const today = new Date().toDateString();
+
+    // pタグ(.date p)に年/月/日/曜日で表示
     document.querySelector('.date p').innerHTML
-    = new Date().toDateString();
-    
+    = formatDate(today);
+
     let days = "";
     
+    console.log(date.getDate());
+    // 曜日の数値を1まで引いていき、div.prev-dateで表示,
+    // 前月で同じ週の日にちを表示
     for(let x = firstDayIndex; x > 0; x--){
+        // 配列で０スタートなので+1する
         days += `<div class="prev-date">${prevLastDay - x + 1}
         </div>`;
     }
